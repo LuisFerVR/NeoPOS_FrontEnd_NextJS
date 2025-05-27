23895e56562345c0a931d5f815c6a79977f38422
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-05-27 15:12:39 -0600

    Se hizo el componente de ShoppingCart , ShoppingCartItem que se encarga de renderizar cada producto agregado al carrito y se creó el type CartItem para tipar  el item recibido en ShoppingCartItem.

--- components/cart/ShoppingCart.tsx
+++ components/cart/ShoppingCart.tsx
@@ -1,6 +1,22 @@
+'use client';
+
+import { useStore } from "@/app/src/store";
+import ShoppingCartItem from "./ShoppingCarItem";
 
 export default function ShoppingCart() {
+  const contents = useStore((state) => state.contents);
+
   return (
-    <h2 className="text-4xl font-bold text-gary-900">Resumen de Venta</h2>
+    <>
+      <h2 className="text-4xl font-bold text-gary-900">Resumen de Venta</h2>
+      <ul className="mt-6 divide-y divide-gar-200 border-t border-gary-200 text-sm font-medium text-gray-500">
+        {contents.map((item) => (
+          <ShoppingCartItem 
+            key={item.productId}
+            item={item}
+          />
+        ))}
+      </ul>
+    </>
   )
 }
