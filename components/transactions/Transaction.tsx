b503755fa441d5f9f9ab1a87b72c8050122280b0
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-05-30 14:43:46 -0600

    Se mostró el contenido e imagen de las ventas realizadas.
    Se calculó y mostró el total d eventas por día.
    
    Se resolvió el error de hidratación en Transaction al usar Calendar de react-calendar, s esolucionó agregandole la prop locale="es-ES" al componente Calendar al usar su etiqueta.

--- components/transactions/Transaction.tsx
+++ components/transactions/Transaction.tsx
@@ -6,28 +6,37 @@ import "react-calendar/dist/Calendar.css";
 import { format } from "date-fns";
 import { useQuery } from "@tanstack/react-query";
 import { getSalesByDate } from "@/app/src/api";
+import TransactionSummary from "./TrnsactionSummary";
+import { formatCurrency } from "@/app/src/utils";
 type valuePiece = Date | null;
 type Value = valuePiece | [valuePiece, valuePiece];
 
 export default function TransactionFilter() {
     const [date, setDate] = useState<Value>(new Date());
     const formattedDate = format(date?.toString() || new Date(), 'dd/MM/yyyy');
-    console.log(`Fecha seleccionada: ${formattedDate}`);
     const { data, isLoading } = useQuery({
         queryKey: ['sales', formattedDate],
         queryFn: () => getSalesByDate(formattedDate),
     })
+    const total = data?.reduce((total, transaction) => total + +transaction.total, 0) ?? 0;
 
-  return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-10">
-        <div>
+   return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-10 relative items-start">
+        <div className="lg:sticky lg:top-10">
             <Calendar 
                 value={date}
                 onChange={setDate}
+                locale="es-ES"
             />
         </div>
         <div>
-
+            {isLoading && <p className="text-lg text-center">Cargando ventas...</p>}
+            {data ? data.length ? data.map( transaction => (
+                <TransactionSummary key={transaction.id} transaction={transaction}/>
+            )): <p className="text-lg text-center">No hay ventas en esta fecha</p>: null}
+            <p className="my-5 text-lg font-bold text-right">Total del día: {''}
+                <span className="font-normal">{formatCurrency(total)}</span>
+            </p>
         </div>
     </div>
   )
