024f774783abfca08f0f676dbf2c6df47267c925
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-06-07 19:59:58 -0600

    fix de proyecto para publicación

--- app/src/api.tsx
+++ app/src/api.tsx
@@ -3,7 +3,9 @@ import { TransactionsResponseSchema } from "./schemas";
 export async function getSalesByDate( date: string) {
     const url = `${process.env.NEXT_PUBLIC_DOMAIN}/admin/sales/api?transactionDate=${date}`;
     const req = await fetch(url);
+    console.log('Fetching sales data:', req);
     const res = await req.json();
     const transactions = TransactionsResponseSchema.parse(res);
+    console.log('Transactions:', transactions);
     return transactions;
 }
--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
-      }
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+      },
     ]
   }
 };
