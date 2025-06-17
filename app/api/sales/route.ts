cc4f7a983c8c0d1708902c6bcbce9840887d7e2a
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-06-17 16:04:51 -0600

    fix url getSalesByData en api

--- app/api/sales/route.ts
+++ app/api/sales/route.ts
@@ -3,7 +3,7 @@ import { NextRequest } from "next/server";
 export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams;
     const transactionDate = searchParams.get("transactionDate");
-    const url = `${process.env.API_URL}/api/transactions?transactionDate=${transactionDate}`;
+    const url = `${process.env.NEXT_PUBLIC_API_URL}/transactions?transactionDate=${transactionDate}`;
     const req = await fetch(url);
     const res = await req.json();
     return Response.json(res);
--- app/src/api.tsx
+++ app/src/api.tsx
@@ -1,9 +1,8 @@
 import { TransactionsResponseSchema } from "./schemas";
 
 export async function getSalesByDate( date: string) {
-    const url = `${process.env.NEXT_PUBLIC_DOMAIN}/admin/sales/api?transactionDate=${date}`;
+    const url = `/api/sales?transactionDate=${date}`;
     const req = await fetch(url);
-    console.log('Fetching sales data:', req);
     const res = await req.json();
     const transactions = TransactionsResponseSchema.parse(res);
     console.log('Transactions:', transactions);
